ea211fb0dd9d4d7215e5370f780c1bdfd7aa1c33
philhannant <philip.a.hannant@gmail.com>
2016-07-16 21:15:43 +0100

    added interface, all in wrong order!

--- src/main/java/realTimeSoundCapture/SoundCapture.java
+++ src/main/java/realTimeSoundCapture/SoundCapture.java
@@ -1,25 +1,10 @@
 package realTimeSoundCapture;
 
-import javax.sound.sampled.*;
-import java.io.*;
-
 /**
- * Created by philhannant on 20/06/2016.
+ * Created by philhannant on 16/07/2016.
  */
-public class SoundCapture {
+public interface SoundCapture {
 
-    private AudioFormat format;
-    private float SAMPLE_RATE = 44100;
-    private int BITS_PER_SAMPLE = 16;
-    private int CHANNELS = 2;
-    private boolean SIGNED = true;
-    private boolean BIG_ENDIAN = true;
-    private TargetDataLine input;
-    private AudioInputStream inputStream;
-    private SourceDataLine dataLine;
+    void startCapture();
 
-
-    public SoundCapture(){
-
-    }
 }
