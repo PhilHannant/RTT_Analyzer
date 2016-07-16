bfb28b2cfbc9015d14d708350ef9028272b041e0
philhannant <philip.a.hannant@gmail.com>
2016-07-16 20:31:12 +0100

    variables of soundcapture added

--- src/main/java/realTimeSoundCapture/SoundCapture.java
+++ src/main/java/realTimeSoundCapture/SoundCapture.java
@@ -8,6 +8,18 @@ import java.io.*;
  */
 public class SoundCapture {
 
+    private AudioFormat format;
+    private float SAMPLE_RATE = 44100;
+    private int BITS_PER_SAMPLE = 16;
+    private int CHANNELS = 2;
+    private boolean SIGNED = true;
+    private boolean BIG_ENDIAN = true;
+    private TargetDataLine input;
+    private AudioInputStream inputStream;
+    private SourceDataLine dataLine;
 
 
+    public SoundCapture(){
+
+    }
 }
