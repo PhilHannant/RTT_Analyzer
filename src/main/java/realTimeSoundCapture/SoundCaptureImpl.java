8affc7b53c63542f751fd490d5eb7bb9f461c4d0
philhannant <philip.a.hannant@gmail.com>
2016-07-16 21:44:58 +0100

    start of startcapture added

--- src/main/java/realTimeSoundCapture/SoundCaptureImpl.java
+++ src/main/java/realTimeSoundCapture/SoundCaptureImpl.java
@@ -15,10 +15,14 @@ public class SoundCaptureImpl implements SoundCapture{
     private DataLine dataLine;
     private ByteArrayOutputStream outputStream;
     private int bytesRead;
+    private int streamedBytes;
+    private int sampleSize;
+    private byte[] data;
     private long recordLength = 30000; //30 second length used for testing purposes
 
 
     public SoundCaptureImpl(){
+        sampleSize = 1024;
         float sampleRate = 44100;
         int bitsPerSample = 16;
         int channels = 2;
@@ -27,8 +31,28 @@ public class SoundCaptureImpl implements SoundCapture{
         format = new AudioFormat(sampleRate, bitsPerSample, channels, signed, bigEndian);
     }
 
-    public void startCapture(){
+    public int startCapture(){
+        try {
+            input = AudioSystem.getTargetDataLine(format);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            input = (TargetDataLine) AudioSystem.getLine(info);
+            input.open(format);
+            outputStream = new ByteArrayOutputStream();
 
+            long currentTime = System.currentTimeMillis();
+            long finishTime = currentTime + recordLength;
+            bytesRead = 0;
+            while(currentTime < finishTime){
 
+                streamedBytes = input.read(data, 0, sampleSize);
+                bytesRead += streamedBytes;
+
+                outputStream.write(data, 0, streamedBytes);
+            }
+            return bytesRead;
+        } catch (LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+        return 0;
     }
 }
