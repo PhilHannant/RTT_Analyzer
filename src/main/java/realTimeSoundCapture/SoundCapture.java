edabe64e4c351d1be5f7662c142268ca01c65237
philhannant <philip.a.hannant@gmail.com>
2016-07-18 22:51:31 +0100

    interface updated

--- src/main/java/realTimeSoundCapture/SoundCapture.java
+++ src/main/java/realTimeSoundCapture/SoundCapture.java
@@ -1,5 +1,7 @@
 package realTimeSoundCapture;
 
+import java.io.IOException;
+
 /**
  * Created by philhannant on 16/07/2016.
  */
@@ -7,4 +9,9 @@ public interface SoundCapture {
 
     int startCapture();
 
+    Object getNext();
+
+    void writeNext(Object data);
+
+    void close() throws IOException;
 }
