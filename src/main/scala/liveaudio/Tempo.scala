670a61fc965d7d3e95487e135f9537deb3490323
philhannant <philip.a.hannant@gmail.com>
2016-07-31 22:45:14 +0100

    split analyser into trait and extended case classes

--- src/main/scala/liveaudio/Tempo.scala
+++ src/main/scala/liveaudio/Tempo.scala
@@ -2,16 +2,27 @@ package liveaudio
 
 import scala.collection.mutable.ArrayBuffer
 
+trait Analyser {
+  def addTempo(tempo: Tempo)
+}
 /**
   * Created by philhannant on 31/07/2016.
   */
 case class Tempo(tempo: Double, baseTempo: Double)
 
 
-case class Analyzer(name: String, buffer: ArrayBuffer[Tempo]){
+case class DWTAnalyser(name: String, buffer: ArrayBuffer[Tempo]) extends Analyser{
 
 
   def addTempo(tempo: Tempo) = {
       buffer += tempo
+  }
+}
+
+case class WormAnalyser(name: String, buffer: ArrayBuffer[Tempo]) extends Analyser{
+
+
+  def addTempo(tempo: Tempo) = {
+    buffer += tempo
   }
 }
