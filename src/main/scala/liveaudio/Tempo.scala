f2a748b87385484bb3e87ea5db3c82416e52bdbd
philhannant <philip.a.hannant@gmail.com>
2016-07-31 17:42:43 +0100

    Analyzer case class added, arraybuffer object and add function

--- src/main/scala/liveaudio/Tempo.scala
+++ src/main/scala/liveaudio/Tempo.scala
@@ -1,8 +1,18 @@
 package liveaudio
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by philhannant on 31/07/2016.
   */
-case class Tempo(tempo: Double) {
+case class Tempo(tempo: Double)
 
+
+case class Analyzer(name: String){
+
+  val buffer = new ArrayBuffer[Tempo]
+
+  def addTempo(tempo: Tempo) = {
+      buffer += tempo
+  }
 }
