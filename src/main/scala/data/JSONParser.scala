f0f8f69d6ac9853cbfcfef747770d2856e088e78
philhannant <philip.a.hannant@gmail.com>
2016-07-31 23:02:23 +0100

    json parsing!

---
+++ src/main/scala/data/JSONParser.scala
@@ -0,0 +1,32 @@
+package data
+
+import play.api.libs.json._
+import liveaudio.Tempo
+import liveaudio.DWTAnalyser
+/**
+  * Created by philhannant on 31/07/2016.
+  */
+case class JSONParser() {
+
+
+  def write(obj: DWTAnalyser): String = {
+
+    implicit val tempoWrites = new Writes[Tempo] {
+      def writes(tempo: Tempo) = Json.obj(
+        "tempo" -> tempo.tempo,
+        "expectedTempo" -> tempo.baseTempo
+      )
+    }
+
+    implicit val analyzerWrites = new Writes[DWTAnalyser] {
+      def writes(analyzer: DWTAnalyser) = Json.obj(
+        "name" -> analyzer.name,
+        "buffer" -> analyzer.buffer
+      )
+    }
+
+    val json2 = Json.toJson(obj)
+    println(json2)
+    json2.toString()
+  }
+}
