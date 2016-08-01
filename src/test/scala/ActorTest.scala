d38a6a58edabb6ed481f32af8db6426a6fd4d2a1
philhannant <philip.a.hannant@gmail.com>
2016-08-01 22:52:53 +0100

    basic actor start test

--- src/test/scala/ActorTest.scala
+++ src/test/scala/ActorTest.scala
@@ -1,6 +1,15 @@
+import akka.actor.{ActorSystem, Props}
+import liveaudio.LiveAudioActor
+
 /**
   * Created by philhannant on 01/08/2016.
   */
 object ActorTest extends App{
+
+  val system = ActorSystem("liveaudioActors")
+  val coord = system.actorOf(Props(new LiveAudioActor()))
+  //First message sent to coordinator to begin calculation
+  coord ! start()
+
 
 }
