6b8de340b81bda59fdd57e4210acac945d565242
philhannant <philip.a.hannant@gmail.com>
2016-08-01 22:58:18 +0100

    startLIVEAUDIO message added and working

--- src/main/scala/liveaudio/LiveAudioActor.scala
+++ src/main/scala/liveaudio/LiveAudioActor.scala
@@ -1,8 +1,24 @@
 package liveaudio
 
+
+
+import akka.actor._
+import akka.routing._
+import at.ofai.music.worm.{Worm, WormControlPanel}
+import data.StartLiveAudio
 /**
   * Created by philhannant on 01/08/2016.
   */
-class LiveAudioActor {
-
+class LiveAudioActor extends Actor with ActorLogging{
+   def receive = {
+     case StartLiveAudio =>
+       val w = new Worm
+       val wcp: WormControlPanel = new WormControlPanel(w)
+       wcp.actionPerformed("Play")
+       Thread.sleep(15000)
+       wcp.actionPerformed("Quit")
+       System.out.print("end")
+   }
 }
+
+
