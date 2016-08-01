b2ba51f1bf54cbf4eb725bf67064e4ab0d001277
philhannant <philip.a.hannant@gmail.com>
2016-08-01 21:39:14 +0100

    Tempo refactored to add companion object

--- src/main/scala/liveaudio/Tempo.scala
+++ src/main/scala/liveaudio/Tempo.scala
@@ -8,10 +8,18 @@ trait Analyser {
 /**
   * Created by philhannant on 31/07/2016.
   */
-case class Tempo(tempo: Double, baseTempo: Double){
-  val difference: Double = {baseTempo - tempo}
-}
+case class Tempo(tempo: Double, baseTempo: Double, difference: Double){
 
+}
+object Tempo{
+
+  def apply(tempo: Double, baseTempo: Double): Tempo = {
+    val difference = baseTempo - tempo
+    Tempo(tempo, baseTempo, difference)
+
+  }
+
+}
 
 case class DWTAnalyser(name: String, buffer: ArrayBuffer[Tempo]) extends Analyser{
 
