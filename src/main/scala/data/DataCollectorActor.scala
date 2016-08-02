126b8f6bf565f92875b16dcaa1fd754c7202ac83
philhannant <philip.a.hannant@gmail.com>
2016-08-02 22:25:53 +0100

    adding data collection actor

---
+++ src/main/scala/data/DataCollectorActor.scala
@@ -0,0 +1,13 @@
+package data
+
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorLogging}
+
+/**
+  * Created by philhannant on 02/08/2016.
+  */
+class DataCollectorActor extends Actor with ActorLogging{
+
+  def receive = ???
+
+}
