e8af350a7c1735d766ccf4069ca01f8307f7373b
philhannant <philip.a.hannant@gmail.com>
2016-08-02 22:19:16 +0100

    own file now

--- src/main/scala/data/Tempo.scala
+++ src/main/scala/data/Tempo.scala
@@ -1,13 +1,5 @@
 package data
 
-import java.sql.Date
-import java.text.SimpleDateFormat
-
-import scala.collection.mutable.ArrayBuffer
-
-trait Analyser {
-  def addTempo(tempo: Tempo)
-}
 /**
   * Created by philhannant on 31/07/2016.
   */
@@ -24,40 +16,6 @@ object Tempo{
 
 }
 
-case class DWTAnalyser(name: String, buffer: ArrayBuffer[Tempo]) extends Analyser{
 
 
-  def addTempo(tempo: Tempo) = {
-      buffer += tempo
-  }
-}
 
-object DWTAnalyser{
-  val sdf = new SimpleDateFormat("yyyy-mm-dd, HH:mm:ss")
-
-  def apply (name: String): DWTAnalyser = {
-    val buffer = new ArrayBuffer[Tempo]()
-    val resultDate = new Date(System.currentTimeMillis())
-    val fullName = name + " WormAnalyser: " + sdf.format(resultDate)
-    DWTAnalyser(fullName, buffer)
-  }
-}
-
-
-case class WormAnalyser(name: String, buffer: ArrayBuffer[Tempo]) extends Analyser{
-
-
-  def addTempo(tempo: Tempo) = {
-    buffer += tempo
-  }
-}
-object WormAnalyser{
-  val sdf = new SimpleDateFormat("yyyy-mm-dd, HH:mm:ss")
-
-  def apply (name: String): DWTAnalyser = {
-    val buffer = new ArrayBuffer[Tempo]()
-    val resultDate = new Date(System.currentTimeMillis())
-    val fullName = name + " WormAnalyser: " + sdf.format(resultDate)
-    DWTAnalyser(fullName, buffer)
-  }
-}
