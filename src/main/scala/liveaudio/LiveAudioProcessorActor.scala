5080e5d0a91b2f68d258f14b228753d55891c45a
philhannant <philip.a.hannant@gmail.com>
2016-08-02 20:09:37 +0100

    no longer need soundcaptureimpl to record bytes so creating liveaudioActorProcessor

---
+++ src/main/scala/liveaudio/LiveAudioProcessorActor.scala
@@ -0,0 +1,13 @@
+package liveaudio
+
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorLogging}
+
+/**
+  * Created by philhannant on 02/08/2016.
+  */
+class LiveAudioProcessorActor extends Actor with ActorLogging {
+
+  def receive = ???
+
+}
