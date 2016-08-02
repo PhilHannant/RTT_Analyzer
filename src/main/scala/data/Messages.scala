2f25391c4beed37f52ad46d992a00be6b5f3265a
philhannant <philip.a.hannant@gmail.com>
2016-08-02 22:10:22 +0100

    added newtempo message

--- src/main/scala/data/Messages.scala
+++ src/main/scala/data/Messages.scala
@@ -7,3 +7,4 @@ sealed trait Messages
 case class StartLiveAudio() extends Messages
 case class EndLiveAudio() extends Messages
 case class ProcessBytes(data: Array[Byte]) extends Messages
+case class NewTempo(tempo: Double, expected: Double) extends Messages
