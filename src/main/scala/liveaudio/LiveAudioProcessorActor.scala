87217bcb7bc8fa2b686164cac75d1e14e0b68144
philhannant <philip.a.hannant@gmail.com>
2016-08-02 20:34:55 +0100

    working

--- src/main/scala/liveaudio/LiveAudioProcessorActor.scala
+++ src/main/scala/liveaudio/LiveAudioProcessorActor.scala
@@ -2,12 +2,24 @@ package liveaudio
 
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, ActorLogging}
+import data.{Messages, ProcessBytes}
+import dwtbpm.WaveletBPMDetector
 
 /**
   * Created by philhannant on 02/08/2016.
   */
 class LiveAudioProcessorActor extends Actor with ActorLogging {
 
-  def receive = ???
+  def receive = {
+    case ProcessBytes(data: Array[Byte]) =>
+      println("got it")
+      val ap = new LiveAudioProcessor
+      ap.addData(data)
+      val dwtbpm = new WaveletBPMDetector(
+        ap,
+        131072,
+        WaveletBPMDetector.Daubechies4)
+      val res = dwtbpm.bpm()
+  }
 
 }
