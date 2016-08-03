6168093e38dd29c31501227a6f7a68fbb065c0ab
philhannant <philip.a.hannant@gmail.com>
2016-08-03 22:20:13 +0100

    name change and now holds processBytes

--- src/main/scala/data/ProcessingActor.scala
+++ src/main/scala/data/ProcessingActor.scala
@@ -2,17 +2,27 @@ package data
 
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, ActorLogging}
+import dwtbpm.WaveletBPMDetector
+import liveaudio.LiveAudioProcessor
 
 /**
   * Created by philhannant on 02/08/2016.
   */
-class DataCollectorActor extends Actor with ActorLogging{
+class ProcessingActor extends Actor with ActorLogging{
 
   val wormAnalyser = WormAnalyser(null, null)//placeholders
   val dWtAnalyser = DWTAnalyser(null, null)//placeholders
   val jsonParser = JSONParser()
 
   def receive = {
+    case ProcessBytes(data: Array[Byte]) =>
+      println("got it")
+      val ap = new LiveAudioProcessor
+      ap.addData(data)
+      val dwtbpm = new WaveletBPMDetector(
+        ap,
+        131072,
+        WaveletBPMDetector.Daubechies4).bpm(self)
     case NewTempoDwt(tempo, expected) =>
       val t = Tempo(tempo, expected)
       dWtAnalyser.addTempo(t)
