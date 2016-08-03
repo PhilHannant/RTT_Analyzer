d6d996e021f6a03e72e61bb4d5d8f16c392b75d5
philhannant <philip.a.hannant@gmail.com>
2016-08-03 21:10:54 +0100

    Might not need liveaudioprocesseractor

--- src/main/scala/liveaudio/LiveAudioActor.scala
+++ src/main/scala/liveaudio/LiveAudioActor.scala
@@ -5,7 +5,8 @@ package liveaudio
 import akka.actor._
 import akka.routing._
 import at.ofai.music.worm.{Worm, WormControlPanel}
-import data.{EndLiveAudio, ProcessBytes, StartLiveAudio}
+import data._
+import dwtbpm.WaveletBPMDetector
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,25 +17,27 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class LiveAudioActor extends Actor with ActorLogging{
 
   val soundCaptureImpl = new SoundCaptureImpl
-
+  var w: Worm = new Worm(self)
+  val liveAudioProcessorActor = context.actorOf(Props[LiveAudioProcessorActor], "dwtProcessor")
+  val dataCollectionActor = context.actorOf(Props[DataCollectorActor], "dataCollector")
 
    def receive = {
      case StartLiveAudio =>
-       val w = new Worm(self)
        println(self.toString())
        w.play()
-       Thread.sleep(15000)
-       System.exit(0)
-       System.out.print("end")
      case EndLiveAudio =>
        println("end")
        context.system.terminate()
        System.exit(0)
      case ProcessBytes(data: Array[Byte]) =>
-       var f = Future {
-         soundCaptureImpl.run(data)
-       }
-     case "help" =>
+       println("got it")
+       val ap = new LiveAudioProcessor
+       ap.addData(data)
+       val dwtbpm = new WaveletBPMDetector(
+         ap,
+         131072,
+         WaveletBPMDetector.Daubechies4).bpm(dataCollectionActor)
+     case _ =>
        println("help")
        context.system.terminate()
        System.exit(0)
