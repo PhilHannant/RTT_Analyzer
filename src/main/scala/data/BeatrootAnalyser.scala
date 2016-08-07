efa3db6d926fe0b4e6d7683dbf21061f12b992e3
philhannant <philip.a.hannant@gmail.com>
2016-08-07 16:54:25 +0100

    added beatrootAnalyser

--- src/main/scala/data/BeatrootAnalyser.scala
+++ src/main/scala/data/BeatrootAnalyser.scala
@@ -1,9 +1,28 @@
 package data
 
-/**
-  * Created by philhannant on 07/08/2016.
-  */
-class BeatrootAnalyser extends Analyser {
+import java.sql.Date
+import java.text.SimpleDateFormat
 
-  override def addTempo(tempo: Tempo): Unit = ???
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by philhannant on 02/08/2016.
+  */
+case class BeatrootAnalyser(name: String, buffer: ArrayBuffer[Tempo]) extends Analyser{
+
+
+  def addTempo(tempo: Tempo) = {
+    buffer += tempo
+  }
+}
+
+object BeatrootAnalyser{
+  val sdf = new SimpleDateFormat("yyyy-mm-dd, HH:mm:ss")
+
+  def apply (name: String): BeatrootAnalyser = {
+    val buffer = new ArrayBuffer[Tempo]()
+    val resultDate = new Date(System.currentTimeMillis())
+    val fullName = name + " BeatrootAnalyser: " + sdf.format(resultDate)
+    BeatrootAnalyser(fullName, buffer)
+  }
 }
