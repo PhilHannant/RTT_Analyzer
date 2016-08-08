95f7744165e3ff810bcf1bc583f5840fccbd3812
philhannant <philip.a.hannant@gmail.com>
2016-08-08 22:07:49 +0100

    changes to listbuffer

--- src/main/scala/data/WormAnalyser.scala
+++ src/main/scala/data/WormAnalyser.scala
@@ -3,12 +3,12 @@ package data
 import java.sql.Date
 import java.text.SimpleDateFormat
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 /**
   * Created by philhannant on 02/08/2016.
   */
-case class WormAnalyser(name: String, buffer: ArrayBuffer[Tempo], var stats: Option[Stats]) extends Analyser{
+case class WormAnalyser(name: String, buffer: ListBuffer[Tempo], var stats: Option[Stats]) extends Analyser{
 
   def addTempo(tempo: Tempo) = {
     buffer += tempo
@@ -19,7 +19,7 @@ object WormAnalyser{
   val sdf = new SimpleDateFormat("yyyy-mm-dd, HH:mm:ss")
 
   def apply (name: String): WormAnalyser = {
-    val buffer = new ArrayBuffer[Tempo]()
+    val buffer = new ListBuffer[Tempo]()
     val resultDate = new Date(System.currentTimeMillis())
     val fullName = name + " WormAnalyser: " + sdf.format(resultDate)
     WormAnalyser(fullName, buffer, None)
