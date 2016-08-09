54e15fff0c8c41a25a07308af867689fd9c6e562
philhannant <philip.a.hannant@gmail.com>
2016-08-09 23:06:50 +0100

    missed some nones

--- src/test/scala/LiveAudioProcessorTest.scala
+++ src/test/scala/LiveAudioProcessorTest.scala
@@ -92,24 +92,24 @@ class WaveletBPMDetectorTest extends FlatSpec {
 class TempoObjectTest extends FlatSpec {
 
   "A Tempo object " should "hold a tempo value" in {
-    val t = Tempo(120, 120)
+    val t = Tempo(120, 120, None)
     val result = t.tempo
     val expected = 120
     assertResult(expected)(result)
   }
 
   "A Tempo object" should "calculate the difference between 2 tempos" in {
-    val t = Tempo(119.983827, 120)
+    val t = Tempo(119.983827, 120, None)
     val result = t.difference
     val expected = 120 - 119.983827
     assertResult(expected)(result)
   }
 
   "An Analyzer" should "take multiple tempo objects" in {
-    val t1 = Tempo(115, 120)
-    val t2 = Tempo(125, 120)
-    val t3 = Tempo(135, 120)
-    val t4 = Tempo(145, 120)
+    val t1 = Tempo(115, 120, None)
+    val t2 = Tempo(125, 120, None)
+    val t3 = Tempo(135, 120, None)
+    val t4 = Tempo(145, 120, None)
     val a = DWTAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -127,10 +127,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "takes a class and produce a JSON string" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120)
-    val t2 = Tempo(125, 120)
-    val t3 = Tempo(135, 120)
-    val t4 = Tempo(145, 120)
+    val t1 = Tempo(115, 120, None)
+    val t2 = Tempo(125, 120, None)
+    val t3 = Tempo(135, 120, None)
+    val t4 = Tempo(145, 120, None)
     val a = DWTAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -142,10 +142,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "write a json object to file" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120)
-    val t2 = Tempo(125, 120)
-    val t3 = Tempo(135, 120)
-    val t4 = Tempo(145, 120)
+    val t1 = Tempo(115, 120, None)
+    val t2 = Tempo(125, 120, None)
+    val t3 = Tempo(135, 120, None)
+    val t4 = Tempo(145, 120, None)
     val a = DWTAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -162,10 +162,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "write a wormanalyser object to a json file" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120)
-    val t2 = Tempo(125, 120)
-    val t3 = Tempo(135, 120)
-    val t4 = Tempo(145, 120)
+    val t1 = Tempo(115, 120, None)
+    val t2 = Tempo(125, 120, None)
+    val t3 = Tempo(135, 120, None)
+    val t4 = Tempo(145, 120, None)
     val a = WormAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -182,10 +182,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "merge to json objects together and write to file" in {
     val jp2 = JSONParser()
-    val t1 = Tempo(115, 120)
-    val t2 = Tempo(125, 120)
-    val t3 = Tempo(135, 120)
-    val t4 = Tempo(145, 120)
+    val t1 = Tempo(115, 120, None)
+    val t2 = Tempo(125, 120, None)
+    val t3 = Tempo(135, 120, None)
+    val t4 = Tempo(145, 120, None)
     val a = WormAnalyser("test: a")
     val b = DWTAnalyser("test: b")
     a.addTempo(t1)
@@ -205,8 +205,8 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "take a stats object to" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120)
-    val t2 = Tempo(125, 120)
+    val t1 = Tempo(115, 120, None)
+    val t2 = Tempo(125, 120, None)
     val a = BeatrootAnalyser("test")
     val s = Stats(1, 2, 3, 4)
     a.addTempo(t1)
