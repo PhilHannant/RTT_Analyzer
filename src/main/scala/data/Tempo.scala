d646d07380adc8067a9476ea37323e989f70f5fb
philhannant <philip.a.hannant@gmail.com>
2016-08-09 20:54:39 +0100

    apply adapted to accept option

--- src/main/scala/data/Tempo.scala
+++ src/main/scala/data/Tempo.scala
@@ -3,14 +3,14 @@ package data
 /**
   * Created by philhannant on 31/07/2016.
   */
-case class Tempo(tempo: Double, baseTempo: Double, difference: Double, var beatCount: Option[Stats]){
+case class Tempo(tempo: Double, baseTempo: Double, difference: Double, var beatCount: Option[Double]){
 
 }
 object Tempo{
 
-  def apply(tempo: Double, baseTempo: Double): Tempo = {
+  def apply(tempo: Double, baseTempo: Double, beatCount: Option[Double]): Tempo = {
     val difference = baseTempo - tempo
-    Tempo(tempo, baseTempo, difference, None)
+    Tempo(tempo, baseTempo, difference, beatCount)
 
   }
 
