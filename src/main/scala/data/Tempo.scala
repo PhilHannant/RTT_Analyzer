99ec8f76993764a51ac273d0678e689c06f6c555
philhannant <philip.a.hannant@gmail.com>
2016-08-09 20:36:30 +0100

    optional beatCount added for beatroot message

--- src/main/scala/data/Tempo.scala
+++ src/main/scala/data/Tempo.scala
@@ -3,14 +3,14 @@ package data
 /**
   * Created by philhannant on 31/07/2016.
   */
-case class Tempo(tempo: Double, baseTempo: Double, difference: Double){
+case class Tempo(tempo: Double, baseTempo: Double, difference: Double, var beatCount: Option[Stats]){
 
 }
 object Tempo{
 
   def apply(tempo: Double, baseTempo: Double): Tempo = {
     val difference = baseTempo - tempo
-    Tempo(tempo, baseTempo, difference)
+    Tempo(tempo, baseTempo, difference, None)
 
   }
 
