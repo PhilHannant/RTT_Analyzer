a72cf3bba6537caabdaa32dd19905b1745e9197d
philhannant <philip.a.hannant@gmail.com>
2016-08-10 23:06:35 +0100

    adapted to allow for beatCount

--- src/main/scala/data/Stats.scala
+++ src/main/scala/data/Stats.scala
@@ -3,6 +3,6 @@ package data
 /**
   * Created by philhannant on 07/08/2016.
   */
-case class Stats(averageTempo: Double, medianTempo: Double, averageDiff: Double, medianDiff: Double) {
+case class Stats(averageTempo: Double, medianTempo: Double, averageDiff: Double, medianDiff: Double, totalBeatCount: Double) {
 
 }
