75e9451cd2f3a7dfcd228f28273f69d65b1b3345
philhannant <philip.a.hannant@gmail.com>
2016-08-11 22:55:15 +0100

    starting gui

---
+++ src/main/scala/data/GUI.scala
@@ -0,0 +1,16 @@
+package data
+
+
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.paint.Color._
+import scalafx.scene.shape.Rectangle
+/**
+  * Created by philhannant on 11/08/2016.
+  */
+class GUI {
+
+
+
+}
