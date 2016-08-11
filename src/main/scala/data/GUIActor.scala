53dea56d7ed71f88ac1cd5bd7871ffb798007771
philhannant <philip.a.hannant@gmail.com>
2016-08-11 22:56:57 +0100

    guiactor added

---
+++ src/main/scala/data/GUIActor.scala
@@ -0,0 +1,12 @@
+package data
+
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorLogging}
+
+/**
+  * Created by philhannant on 11/08/2016.
+  */
+class GUIActor extends Actor with ActorLogging{
+
+  def receive: Receive = ???
+}
