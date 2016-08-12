33a78e26fc31ae3e5ecc165ba85ebc4caaf56ced
philhannant <philip.a.hannant@gmail.com>
2016-08-12 20:07:39 +0100

    basic example from scalafx.org added

--- src/main/scala/data/GUI.scala
+++ src/main/scala/data/GUI.scala
@@ -1,16 +1,31 @@
 package data
 
-
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
-import scalafx.scene.paint.Color._
+import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
+
 /**
   * Created by philhannant on 11/08/2016.
   */
-class GUI {
 
 
+object GUI extends JFXApp {
 
+  stage = new JFXApp.PrimaryStage {
+    title = "Hello World"
+    width = 600
+    height = 450
+    scene = new Scene {
+      fill = Color.LIGHTGREEN
+      content = Set(new Rectangle {
+        x = 25
+        y = 40
+        width = 100
+        height = 100
+        fill <== when(hover) choose Color.GREEN otherwise Color.RED
+      })
+    }
+  }
 }
