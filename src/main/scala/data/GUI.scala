f9efe18c9f3336cd2e971a06c1d632387cef0cd7
philhannant <philip.a.hannant@gmail.com>
2016-08-12 21:11:02 +0100

    basic shape started

--- src/main/scala/data/GUI.scala
+++ src/main/scala/data/GUI.scala
@@ -2,10 +2,15 @@ package data
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
-import scalafx.scene.paint.Color
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.{Color, LinearGradient, Stops}
 import scalafx.scene.shape.Rectangle
+import scalafx.scene.text.Text
+import scalafx.scene.paint.Color._
 
 /**
   * Created by philhannant on 11/08/2016.
@@ -20,8 +25,21 @@ object GUI extends JFXApp {
     height = 450
     scene = new Scene {
       content = Set(new Button("Start") {
-
-      })
+        layoutX = 300
+        layoutY = 225
+      }, new HBox {
+        padding = Insets(20)
+        children = Seq(
+          new Text {
+            text = "RTT_ANalyser "
+            style = "-fx-font-size: 36pt"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(DarkGray, Gray))
+          }
+        )
+      }
+      )
     }
   }
 }
