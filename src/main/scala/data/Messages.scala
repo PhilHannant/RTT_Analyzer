68c7a93dda88fae73cb93364b48e602485b3fc8b
philhannant <philip.a.hannant@gmail.com>
2016-08-13 22:58:40 +0100

    added expected bpm to start message

--- src/main/scala/data/Messages.scala
+++ src/main/scala/data/Messages.scala
@@ -4,7 +4,7 @@ package data
   * Created by philhannant on 01/08/2016.
   */
 sealed trait Messages
-case class StartLiveAudio() extends Messages
+case class StartLiveAudio(expectedBPM: Double) extends Messages
 case class EndLiveAudio() extends Messages
 case class ProcessBytes(data: Array[Byte]) extends Messages
 case class NewTempoWorm(tempo: Double, expected: Double) extends Messages
