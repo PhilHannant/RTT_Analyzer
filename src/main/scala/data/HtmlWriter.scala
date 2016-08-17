d2dab5377a8b7330aba1d958162a25bbf6b7d333
philhannant <philip.a.hannant@gmail.com>
2016-08-17 23:19:44 +0100

    working not very robust though

--- src/main/scala/data/HtmlWriter.scala
+++ src/main/scala/data/HtmlWriter.scala
@@ -14,11 +14,18 @@ class HtmlWriter {
 
 
     val heading = worm.getClass.getDeclaredFields
+    val stats = unWrapStats(worm.stats)
+    println(stats)
+    val shead = getStats(worm.stats).getClass.getDeclaredFields
 
     html ++= "<table><thead>"
     html ++= "<tr><th>" + heading(0).getName + "</th>"
-    html ++= "<th>" + heading(2).getName + "</th></tr></thead></table>"
-
+    html ++= "<th>" + heading(2).getName + "</th></tr></thead>"
+    html ++= "<tbody><tr><td>" + worm.name + "</td>"
+    html ++= "<tr><td>" + shead(0).getName + "</td><td>" + stats(0) + "</td></tr>"
+    html ++= "<tr><td>" + shead(1).getName + "</td><td>" + stats(1) + "</td></tr>"
+    html ++= "<tr><td>" + shead(2).getName + "</td><td>" + stats(2) + "</td></tr>"
+    html ++= "<tr><td>" + shead(3).getName + "</td><td>" + stats(3) + "</td></tr></tbody></table>"
 
     html.toString()
   }
@@ -28,6 +35,16 @@ class HtmlWriter {
     file.write(html.toString())
     file.flush()
     file.close()
+  }
+
+  def unWrapStats(stats: Option[Stats]): List[Double] = stats match {
+    case None => Nil
+    case Some(x) => List(x.averageTempo, x.medianTempo, x.averageDiff, x.medianDiff, x.totalBeatCount)
+  }
+
+  def getStats(stats: Option[Stats]): Stats = stats match {
+    case Some(x) => x
+
   }
 
 }
