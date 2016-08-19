9aae8f5132756035e4bd5df02a5c08e615381de1
philhannant <philip.a.hannant@gmail.com>
2016-08-20 00:23:50 +0100

    adding in getResponse time calc

--- src/test/scala/LiveAudioProcessorTest.scala
+++ src/test/scala/LiveAudioProcessorTest.scala
@@ -92,24 +92,24 @@ class WaveletBPMDetectorTest extends FlatSpec {
 class TempoObjectTest extends FlatSpec {
 
   "A Tempo object " should "hold a tempo value" in {
-    val t = Tempo(120, 120, None)
+    val t = Tempo(120, 120, None, System.currentTimeMillis())
     val result = t.tempo
     val expected = 120
     assertResult(expected)(result)
   }
 
   "A Tempo object" should "calculate the difference between 2 tempos" in {
-    val t = Tempo(119.983827, 120, None)
+    val t = Tempo(119.983827, 120, None, System.currentTimeMillis())
     val result = t.difference
     val expected = 120 - 119.983827
     assertResult(expected)(result)
   }
 
   "An Analyzer" should "take multiple tempo objects" in {
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     val a = DWTAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -127,10 +127,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "takes a class and produce a JSON string" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     val a = DWTAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -142,10 +142,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "write a json object to file" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     val a = DWTAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -162,10 +162,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "write a wormanalyser object to a json file" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     val a = WormAnalyser("test")
     a.addTempo(t1)
     a.addTempo(t2)
@@ -182,10 +182,10 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "merge to json objects together and write to file" in {
     val jp2 = JSONParser()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     val a = WormAnalyser("test: a")
     val b = DWTAnalyser("test: b")
     a.addTempo(t1)
@@ -205,8 +205,8 @@ class JSONParserTest extends FlatSpec {
 
   "A JSONParser" should "take a stats object to" in {
     val jp = JSONParser()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
     val a = BeatrootAnalyser("test")
     val s = Stats(1, 2, 3, 4, 4)
     a.addTempo(t1)
@@ -227,10 +227,10 @@ class JSONParserTest extends FlatSpec {
     val b = BeatrootAnalyser("bt")
     val s = StatsCalculator()
     val buff = new ListBuffer[Tempo]()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     buff += t1
     buff += t2
     buff += t3
@@ -270,10 +270,10 @@ class JSONParserTest extends FlatSpec {
     "A StatsCalculator" should "return a median of a listbuffer" in {
       val s = new StatsCalculator
       val buff = new ListBuffer[Tempo]()
-      val t1 = Tempo(115, 120, None)
-      val t2 = Tempo(125, 120, None)
-      val t3 = Tempo(135, 120, None)
-      val t4 = Tempo(145, 120, None)
+      val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+      val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+      val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+      val t4 = Tempo(145, 120, None, System.currentTimeMillis())
       buff += t1
       buff += t2
       buff += t3
@@ -285,7 +285,10 @@ class JSONParserTest extends FlatSpec {
 
     "A StatsCalculator" should "return a list of tempo values" in {
       val s = new StatsCalculator
-      val buff = List[Tempo](Tempo(115, 120, None), Tempo(125, 120, None), Tempo(135, 120, None), Tempo(145, 120, None))
+      val buff = List[Tempo](Tempo(115, 120, None, System.currentTimeMillis()),
+        Tempo(125, 120, None, System.currentTimeMillis()),
+        Tempo(135, 120, None, System.currentTimeMillis()),
+        Tempo(145, 120, None, System.currentTimeMillis()))
       val result = s.getTempos(buff)
       val expected = List[Double](115, 125, 135, 145)
       assertResult(expected)(result)
@@ -293,7 +296,10 @@ class JSONParserTest extends FlatSpec {
 
     "A StatsCalculator" should "return a list if difference values" in {
       val s = new StatsCalculator
-      val buff = List[Tempo](Tempo(115, 120, None), Tempo(125, 112, None), Tempo(135, 121, None), Tempo(145, 132, None))
+      val buff = List[Tempo](Tempo(115, 120, None, System.currentTimeMillis()),
+        Tempo(125, 112, None, System.currentTimeMillis()),
+        Tempo(135, 121, None, System.currentTimeMillis()),
+        Tempo(145, 132, None, System.currentTimeMillis()))
       val result = s.getDiffs(buff)
       val expected = List[Double](120-115, 112-125, 121-135, 132-145)
       assertResult(expected)(result)
@@ -302,10 +308,10 @@ class JSONParserTest extends FlatSpec {
     "A StatsCalculator" should "return an average" in {
       val s = new StatsCalculator
       val buff = new ListBuffer[Tempo]()
-      val t1 = Tempo(115, 120, None)
-      val t2 = Tempo(125, 120, None)
-      val t3 = Tempo(135, 120, None)
-      val t4 = Tempo(145, 120, None)
+      val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+      val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+      val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+      val t4 = Tempo(145, 120, None, System.currentTimeMillis())
       buff += t1
       buff += t2
       buff += t3
@@ -318,10 +324,10 @@ class JSONParserTest extends FlatSpec {
     "A StatsCalculator" should "return the total beatCount" in {
       val s = new StatsCalculator
       val buff = new ListBuffer[Tempo]()
-      val t1 = Tempo(115, 120, Some(4))
-      val t2 = Tempo(125, 120, Some(3))
-      val t3 = Tempo(135, 120, Some(5))
-      val t4 = Tempo(145, 120, Some(9))
+      val t1 = Tempo(115, 120, Some(4), System.currentTimeMillis())
+      val t2 = Tempo(125, 120, Some(3), System.currentTimeMillis())
+      val t3 = Tempo(135, 120, Some(5), System.currentTimeMillis())
+      val t4 = Tempo(145, 120, Some(9), System.currentTimeMillis())
       buff += t1
       buff += t2
       buff += t3
@@ -331,6 +337,21 @@ class JSONParserTest extends FlatSpec {
       assertResult(expected)(result)
     }
 
+    "A StatsCalculator" should "find the reponseTime" in {
+      val s = new StatsCalculator
+      val buff = new ListBuffer[Tempo]()
+      val t1 = Tempo(115, 120, Some(4), 110)
+      val t2 = Tempo(125, 120, Some(3), System.currentTimeMillis())
+      val t3 = Tempo(119.5, 120, Some(5), 300)
+      val t4 = Tempo(145, 120, Some(9), System.currentTimeMillis())
+      buff += t1
+      buff += t2
+      buff += t3
+      buff += t4
+      val result = s.getResponseTime(buff)
+      val expected = 300
+      assertResult(expected)(result)
+    }
   }
 
 class HtmlWriterTest extends FlatSpec{
@@ -342,10 +363,10 @@ class HtmlWriterTest extends FlatSpec{
     val b = BeatrootAnalyser("bt")
     val s = StatsCalculator()
     val buff = new ListBuffer[Tempo]()
-    val t1 = Tempo(115, 120, None)
-    val t2 = Tempo(125, 120, None)
-    val t3 = Tempo(135, 120, None)
-    val t4 = Tempo(145, 120, None)
+    val t1 = Tempo(115, 120, None, System.currentTimeMillis())
+    val t2 = Tempo(125, 120, None, System.currentTimeMillis())
+    val t3 = Tempo(135, 120, None, System.currentTimeMillis())
+    val t4 = Tempo(145, 120, None, System.currentTimeMillis())
     buff += t1
     buff += t2
     buff += t3
