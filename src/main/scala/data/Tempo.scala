4f0411f63ef0f6271de55518b3cf5a589ce28697
philhannant <philip.a.hannant@gmail.com>
2016-08-19 23:56:27 +0100

    adding timeelapsed parameter

--- src/main/scala/data/Tempo.scala
+++ src/main/scala/data/Tempo.scala
@@ -3,14 +3,14 @@ package data
 /**
   * Created by philhannant on 31/07/2016.
   */
-case class Tempo(tempo: Double, baseTempo: Double, difference: Double, var beatCount: Option[Double]){
+case class Tempo(tempo: Double, baseTempo: Double, difference: Double, var beatCount: Option[Double], timeElapsed: Long){
 
 }
 object Tempo{
 
-  def apply(tempo: Double, baseTempo: Double, beatCount: Option[Double]): Tempo = {
+  def apply(tempo: Double, baseTempo: Double, beatCount: Option[Double], timeElapsed: Long): Tempo = {
     val difference = baseTempo - tempo
-    Tempo(tempo, baseTempo, difference, beatCount)
+    Tempo(tempo, baseTempo, difference, beatCount, timeElapsed)
 
   }
 
