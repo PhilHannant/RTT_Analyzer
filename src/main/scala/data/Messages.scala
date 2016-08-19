41fbc0acb8feb7a0d6e3e7d3fe18c4e705154bdb
philhannant <philip.a.hannant@gmail.com>
2016-08-20 00:01:23 +0100

    adding current time

--- src/main/scala/data/Messages.scala
+++ src/main/scala/data/Messages.scala
@@ -10,9 +10,9 @@ sealed trait Messages
 case class StartLiveAudio(expectedBPM: Double, processingActor: ActorRef, filePath: String, startTime: Long) extends Messages
 case class EndLiveAudio(processingActor: ActorRef) extends Messages
 case class ProcessBytes(data: Array[Byte]) extends Messages
-case class NewTempoWorm(tempo: Double) extends Messages
-case class NewTempoDwt(tempo: Double) extends Messages
-case class NewTempoBeatroot(tempo: Double, beatCount: Double) extends Messages
+case class NewTempoWorm(tempo: Double, currentTime: Long) extends Messages
+case class NewTempoDwt(tempo: Double, currentTime: Long) extends Messages
+case class NewTempoBeatroot(tempo: Double, beatCount: Double, currentTime: Long) extends Messages
 case class ParseJSON() extends Messages
 case class WriteStatsJSON() extends Messages
 case class SendInputs(bpm: Double, filePath: String, startTime: Long) extends Messages
