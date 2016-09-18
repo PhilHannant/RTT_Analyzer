4ab4898c67b7a02bd8a34795739b9f569392cfa0
philhannant <philip.a.hannant@gmail.com>
2016-09-18 22:25:53 +0100

    adding comments

--- src/main/scala/liveaudio/LiveAudioActor.scala
+++ src/main/scala/liveaudio/LiveAudioActor.scala
@@ -12,32 +12,31 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
-  * Created by philhannant on 01/08/2016.
+  * @author Phil Hannant for MSc Computer Science project
+  *
+  * RTT_Analyser LiveAudioActor responsible for starting audio capture and closing system
   */
+
+
 class LiveAudioActor extends Actor with ActorLogging{
 
   val w: Worm = new Worm()
 
-
    def receive = {
      case StartLiveAudio(expectedBPM, processingActor, filePath, startTime) =>
        w.setActor(processingActor)
-       println(self.toString())
        w.play()
        processingActor ! SendInputs(expectedBPM, filePath, startTime)
      case EndLiveAudio(processingActor) =>
        w.stop()
        Thread.sleep(2000)//allow for any processing to finish
        processingActor ! WriteData
-//       processingActor ! Reset
+       println("Test Complete")
      case Close =>
        context.system.terminate()
        System.exit(0)
-
      case _ =>
-       println("help")
-
-
+       println("Message Error")
    }
 }
 
