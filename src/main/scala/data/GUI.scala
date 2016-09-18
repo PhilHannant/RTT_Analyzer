7e103030ed7f639f730e95323772904e09de7bf2
philhannant <philip.a.hannant@gmail.com>
2016-09-18 21:51:17 +0100

    adding comments

--- src/main/scala/data/GUI.scala
+++ src/main/scala/data/GUI.scala
@@ -7,23 +7,21 @@ import akka.actor.{ActorSystem, PoisonPill, Props}
 import liveaudio.LiveAudioActor
 
 import scalafx.Includes._
-import scalafx.application.JFXApp.PrimaryStage
 import scalafx.application.{JFXApp, Platform}
 import scalafx.event.ActionEvent
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
 import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.control._
-import scalafx.scene.effect.DropShadow
 import scalafx.scene.layout._
 import scalafx.scene.paint.{Color, LinearGradient, Stops}
-import scalafx.scene.shape.Rectangle
 import scalafx.scene.text.Text
 import scalafx.scene.paint.Color._
 import scalafx.stage.FileChooser
 
 /**
-  * Created by philhannant on 11/08/2016.
+  * @author Phil Hannant for MSc Computer Science project
+  * RTT_Analyser GUI object
   */
 
 
@@ -45,41 +43,25 @@ object GUI extends JFXApp {
   val testButton = new Button("Test")
   testButton.prefWidth = 100
 
-//  val enterBpm = new Label("")
-//  enterBpm.setTextFill(DarkGray)
-  //        enterBpm.layoutX = 200
-  //        enterBpm.layoutY = 100
-
   val expectedBpm = new TextField
-  //        expectedBpm.layoutX = 300
-  //        expectedBpm.layoutY = 100
   expectedBpm.promptText = "Expected BPM"
   expectedBpm.prefWidth = 250
 
   val fileNameLabel = new Label("")
   fileNameLabel.setTextFill(DarkGray)
-  //        fileName.layoutX = 200
-  //        fileName.layoutY = 100
+
   val fileChooser = new FileChooser()
   val getFilePath = new Button("Set File Name")
   getFilePath.prefWidth = 250
 
   val fileName = new TextField
 
-  //        val controlBar = new ButtonBar {id = "buttonBar"; buttons = List(startButton, stopButton)}
-  //        controlBar.layoutX = 300
-  //        controlBar.layoutY = 425
-  //        controlBar.minWidth = 400
-
   val openResults = new Button("Results")
-
-
 
   val settingsBox = new VBox
   settingsBox.id = "input"
   settingsBox.children = List(expectedBpm, getFilePath, fileNameLabel)
   settingsBox.prefWidth = 350
-
 
   val headingBox = new HBox
   headingBox.id = "heading"
@@ -93,13 +75,10 @@ object GUI extends JFXApp {
         stops = Stops(DarkGray, Gray))
     }, settingsBox)
 
-
-
   val controlBar = new HBox(startButton,testButton, stopButton, openResults, exitButton)
   controlBar.prefWidth = 800
   controlBar.spacing = 25
   controlBar.id = "controlBar"
-
 
   val beatRootLabel = new Label("Beatroot")
   beatRootLabel.id = "tempo1"
@@ -123,19 +102,10 @@ object GUI extends JFXApp {
   val wBpm = new Label("bpm")
   wBpm.id = "tempo3"
 
- // val pb = new ProgressBar()
- // pb.setProgress(-1.0)
-
-
-
   val gridPane = new GridPane
   gridPane.padding = Insets(0, 0, 0, 50)
   gridPane.hgap = 45
   gridPane.vgap = 35
- // gridPane.add(enterBpm, 1, 0)
-//  gridPane.add(fileNameLabel, 4, 0)
- // gridPane.add(expectedBpm, 2, 0)
-//  gridPane.add(getFilePath, 5, 0)
   gridPane.add(beatRootLabel, 1, 1)
   gridPane.add(beatRootTempo, 2, 1)
   gridPane.add(brBpm, 4, 1)
@@ -145,20 +115,18 @@ object GUI extends JFXApp {
   gridPane.add(wormLabel, 1, 3)
   gridPane.add(wormTempo, 2, 3)
   gridPane.add(wBpm, 4, 3)
-  //gridPane.add(pb, 3, 4)
 
   val bPane = new BorderPane
   bPane.setBottom(controlBar)
   bPane.setTop(headingBox)
   bPane.setCenter(gridPane)
 
-  //content = List(headingBox, enterBpm, expectedBpm, bPane)
-
-
+  /** Start button action event */
   startButton.onAction = (e: ActionEvent) => {
     start
   }
 
+  /** Stop button action event */
   stopButton.onAction = (e: ActionEvent) => {
     if (!state) {
       error3.showAndWait()
@@ -167,7 +135,7 @@ object GUI extends JFXApp {
       htmlReady = true
     }
   }
-
+  /** Results button action event */
   openResults.onAction = (e: ActionEvent) => {
     if (!htmlReady) {
       error2.showAndWait()
@@ -177,12 +145,13 @@ object GUI extends JFXApp {
       Desktop.getDesktop().browse(new URL(url).toURI())
     }
   }
-
+  /** Exit button action event */
   exitButton.onAction = (e: ActionEvent) => {
     if (!state) System.exit(0)
     else Operator.opActors.liveAudioActor ! Close
   }
 
+  /** Test button action event */
   testButton.onAction = (e: ActionEvent) => {
     if (filePath == "") {
       error.showAndWait()
@@ -191,7 +160,7 @@ object GUI extends JFXApp {
       Operator.opActors.guiActor ! StartTestTimer(Operator.opActors.processingActor, Operator.opActors.liveAudioActor)
     }
   }
-
+  /** Get file path action event */
   getFilePath.onAction = (e: ActionEvent) => {
     val path = fileChooser.showSaveDialog(stage)
     filePath = path.toString
@@ -212,6 +181,7 @@ object GUI extends JFXApp {
   error3.setTitle("Error Message")
   error3.setContentText("Please press Start or Test to proceed")
 
+  /** set up of scalaFX scene */
   stage = new JFXApp.PrimaryStage {
     title = "RTT_Analyser"
     width = 800
@@ -223,28 +193,27 @@ object GUI extends JFXApp {
     }
 
   }
-
+  /** Updates beatroot tempo value */
   def updatebrt(tempo: Double, count: Double) = {
     beatCount = beatCount + count
     Platform.runLater{
       beatRootTempo.text = f"$tempo%1.2f"
-//      beatRootCount.text = f"$count%1.2f"
     }
   }
-
+  /** Updates dwt tempo value */
   def updateDwt(tempo: Double) = {
     Platform.runLater{
       dwtTempo.text = f"$tempo%1.2f"
     }
   }
-
+  /** Updates worm tempo value */
   def updateWorm(tempo: Double) = {
     Platform.runLater{
       wormTempo.text = f"$tempo%1.2f"
     }
   }
 
-
+  /** Starts live audio capture and processing */
   def start = {
     if (expectedBpm.getText == "") expectedBpm.text = "0"
     if (filePath == "") {
@@ -263,16 +232,23 @@ object GUI extends JFXApp {
 
 }
 
+
+/**
+  * Operator object acts as main class for RTT_Analyser
+  */
+
 object Operator extends App {
 
   var opActors: OperatorActors = _
   var time: Long = _
 
+  /** instantiate actors */
   def setup = {
     opActors = new OperatorActors
     time = System.currentTimeMillis()
   }
 
+  /** Kills actors */
   def reset = {
     opActors.liveAudioActor ! PoisonPill
     opActors.beatrootActor ! PoisonPill
@@ -287,6 +263,11 @@ object Operator extends App {
 
 
 }
+
+
+/**
+  * OperatorActors allows for resetting of RTT_Analyser actors for multiple tests
+  */
 
 class OperatorActors {
 
