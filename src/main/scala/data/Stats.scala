fbd9192f6dba92952d958d766c8ea4183e0c86a0
philhannant <philip.a.hannant@gmail.com>
2016-09-18 21:57:48 +0100

    adding comments

--- src/main/scala/data/Stats.scala
+++ src/main/scala/data/Stats.scala
@@ -1,8 +1,15 @@
 package data
 
 /**
-  * Created by philhannant on 07/08/2016.
+  * @author Phil Hannant for MSc Computer Science project
+  *
+  * RTT_Analyser Stats case class
   */
-case class Stats(averageTempo: Double, medianTempo: Double, averageDiff: Double, medianDiff: Double, totalBeatCount: Double, responseTime: Long) {
 
-}
+case class Stats(averageTempo: Double,
+                 medianTempo: Double,
+                 averageDiff: Double,
+                 medianDiff: Double,
+                 totalBeatCount: Double,
+                 responseTime: Long)
+
